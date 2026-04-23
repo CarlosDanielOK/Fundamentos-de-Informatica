a8c116de20e71dad41ff6ce56b1823cc2181cd10
Carlos <carlosdaniellazo06@gmail.com>
2026-04-23 01:36:43 -0300

    Refactor variables y cálculos para el análisis de tiempos de carrera

--- actividad8.py
+++ actividad8.py
@@ -1,6 +1,6 @@
-cont_1 = 0
-cont_2 = 0
-cont_3 = 0
+menos48 = 0
+entre48105 = 0
+mas105 = 0
 suma = 0
 
 for i in range(8):
@@ -10,15 +10,19 @@ for i in range(8):
         tiempo = int(input("Ingrese el tiempo de su carrera en minutos: "))
 
     if tiempo < 48:
-        cont_1 = cont_1 + 1
-        suma = suma + tiempo
+        menos48 = menos48 + 1
     elif tiempo >= 48 and tiempo <= 105:
-        cont_2 = cont_2 + 1
-        suma = suma + tiempo
-    else:
-        cont_3 = cont_3 + 1
-        suma = suma + tiempo
+        entre48105 = entre48105 + 1
+    elif tiempo > 105:
+        mas105 = mas105 + 1
+    
+    suma = suma + tiempo
 
-print("Porcentaje que tardo menos de 48 minutos:", cont_1 * 100 / 8)
-print("Porcentaje que tardo entre 45 y 105 minutos:", cont_2 * 100 / 8)
-print("Porcentaje que tardo mas de 145 minutos:", cont_3 * 100 / 8)
+print("Porcentaje que tardo menos de 48 minutos:", menos48 * 100 / 8, "%")
+print("Porcentaje que tardo entre 48 y 105 minutos:", entre48105 * 100 / 8, "%")
+print("Porcentaje que tardo mas de 105 minutos:", mas105 * 100 / 8, "%")
+
+promedio = suma / 8
+
+if promedio < 101:
+    print("NUEVO RECORD")
